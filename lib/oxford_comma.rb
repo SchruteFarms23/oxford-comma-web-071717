d1cd216124c1ea373c8bc361f435b166e62b3e54
SchruteFarms23 <romanmullady@gmail.com>
2017-07-11 23:14:36 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+	if array.length > 2
+	array.push("and #{array.pop}")
+  array = array.join(", ")
+  array
+elsif array.length == 2
+	array= array.join(" and ")
+	array
+elsif array.length == 1
+	array.join
+end
 
 end
